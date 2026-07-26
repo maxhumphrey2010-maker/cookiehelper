8e413594662deed507ea26a6240a0a4374592538
maxhumphrey2010-maker <maxhumphrey2010@gmail.com>
2026-07-26 16:52:24 -0500

    Create automation.js

---
+++ automation.js
@@ -0,0 +1,200 @@
+// Cookie Helper+
+// automation.js
+// Automation features
+
+(function () {
+    "use strict";
+
+
+    const CH = window.CookieHelper;
+
+
+    if (!CH) {
+        console.error(
+            "Cookie Helper core missing!"
+        );
+        return;
+    }
+
+
+    CH.modules.automation = {};
+
+
+
+    // Auto click big cookie
+    CH.modules.automation.autoClick = function () {
+
+        if (
+            CH.utils.enabled("autoClick")
+            && typeof Game !== "undefined"
+        ) {
+
+            Game.ClickCookie();
+
+        }
+
+    };
+
+
+
+    // Auto click golden cookies and other shimmers
+    CH.modules.automation.autoGolden = function () {
+
+        if (
+            CH.utils.enabled("autoGolden")
+            && typeof Game !== "undefined"
+        ) {
+
+            Game.shimmers.forEach(shimmer => {
+
+                if (
+                    shimmer.type === "golden"
+                ) {
+
+                    shimmer.pop();
+
+                }
+
+            });
+
+        }
+
+    };
+
+
+
+    // Auto click reindeer
+    CH.modules.automation.autoReindeer = function () {
+
+        if (
+            CH.utils.enabled("autoReindeer")
+            && typeof Game !== "undefined"
+        ) {
+
+            Game.shimmers.forEach(shimmer => {
+
+                if (
+                    shimmer.type === "reindeer"
+                ) {
+
+                    shimmer.pop();
+
+                }
+
+            });
+
+        }
+
+    };
+
+
+
+    // Buy available upgrades
+    CH.modules.automation.autoUpgrade = function () {
+
+        if (
+            CH.utils.enabled("autoUpgrade")
+            && typeof Game !== "undefined"
+        ) {
+
+
+            Game.UpgradesInStore.forEach(upgrade => {
+
+                if (
+                    upgrade.canBuy()
+                ) {
+
+                    upgrade.buy();
+
+                }
+
+            });
+
+        }
+
+    };
+
+
+
+    // Buy affordable buildings
+    CH.modules.automation.autoBuild = function () {
+
+        if (
+            CH.utils.enabled("autoBuild")
+            && typeof Game !== "undefined"
+        ) {
+
+
+            let buildings =
+                Game.ObjectsById.filter(
+                    building =>
+                    building.getPrice()
+                    <= Game.cookies
+                );
+
+
+            if (buildings.length > 0) {
+
+
+                buildings.sort(
+                    (a, b) =>
+                    b.storedCps -
+                    a.storedCps
+                );
+
+
+                buildings[0].buy();
+
+            }
+
+        }
+
+    };
+
+
+
+    // Start automation loops
+    CH.modules.automation.start = function () {
+
+
+        setInterval(() => {
+
+            CH.modules.automation.autoClick();
+
+        }, CH.settings.clickSpeed);
+
+
+
+        setInterval(() => {
+
+            CH.modules.automation.autoGolden();
+
+            CH.modules.automation.autoReindeer();
+
+        }, 100);
+
+
+
+        setInterval(() => {
+
+            CH.modules.automation.autoUpgrade();
+
+            CH.modules.automation.autoBuild();
+
+        }, 1000);
+
+
+
+        CH.utils.log(
+            "Automation system started."
+        );
+
+    };
+
+
+
+    // Start when mod loads
+    CH.modules.automation.start();
+
+
+})();
